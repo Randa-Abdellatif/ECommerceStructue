551da4ba9f5864c1f1af9558ad9ee78147a59f15
Randa-Abdellatif <randaabdellatif105@yahoo.com>
2023-09-04 15:53:16 +0300

    add welcome

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ import initApp from './src/index.router.js'
 const app = express()
 // setup port and the baseUrl
 const port = process.env.PORT || 5000
+app.get('/', (req, res, next) => {
+    res.json({message:"welcome"})
+})
 initApp(app ,express)
 app.listen(port, () => console.log(chalk.bold.blueBright(`Example app listening on port ${port}!`)))
 
